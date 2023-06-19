613d0f7a5d819d530f34f20f18472002ac0fc9fc
Shreyansh Agrawal <80638949+shrey9393@users.noreply.github.com>
2023-06-20 01:22:15 +0530

    data

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import "./index.css";
+import { ProductList } from "./features/product-list/ProductList";
+
+function App() {
+  return <ProductList />;
+}
+
+export default App;
